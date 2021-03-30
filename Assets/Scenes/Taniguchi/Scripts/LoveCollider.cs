e2ad1e4232be7a20f726cd36b48e49062d6d4d9c
KoshiroShiraki <k22946989@gmail.com>
2021-03-30 16:27:56 +0900

    最後

--- Assets/Scenes/Taniguchi/Scripts/LoveCollider.cs
+++ Assets/Scenes/Taniguchi/Scripts/LoveCollider.cs
@@ -7,19 +7,17 @@ public class LoveCollider : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Animal_kusa")
+        if (other.tag == "grass")
         {
             //otherオブジェクトを削除
             other.gameObject.SetActive(false);
             Debug.Log("草に接触");
-            this.gameObject.SetActive(false);
         }
-        else if (other.tag == "Animal_niku")
+        else if (other.tag == "Food")
         {
             //otherオブジェクトを削除
             other.gameObject.SetActive(false);
             Debug.Log("動物に接触");
-            this.gameObject.SetActive(false);
         }
     }
 }
