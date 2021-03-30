0fe3843037527ef42240bd6c2e0a1296c2c9e9e1
Kazumasa Taniguchi <kazumasa22519@gmail.com>
2021-03-30 16:12:23 +0900

    ハート消える処理

--- Assets/Scenes/Taniguchi/Scripts/LoveCollider.cs
+++ Assets/Scenes/Taniguchi/Scripts/LoveCollider.cs
@@ -12,12 +12,14 @@ public class LoveCollider : MonoBehaviour
             //otherオブジェクトを削除
             other.gameObject.SetActive(false);
             Debug.Log("草に接触");
+            this.gameObject.SetActive(false);
         }
         else if (other.tag == "Food")
         {
             //otherオブジェクトを削除
             other.gameObject.SetActive(false);
             Debug.Log("動物に接触");
+            this.gameObject.SetActive(false);
         }
     }
 }
